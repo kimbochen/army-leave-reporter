0f4a0abc4b49e89ede150ec5e9a8abe73a7bc86a
kimbochen <github@thc.anonaddy.com>
2022-01-15 13:31:49 +0800

    Changed to Flask app design.

--- leave_reporter/chatbot.py
+++ leave_reporter/chatbot.py
@@ -1,18 +1,22 @@
 import os
 import logging
 
+from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 
 
+APP = Flask(__name__)
 LINE_BOT_API = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 HANDLER = WebhookHandler(os.environ['CHANNEL_SECRET'])
+
 
 @HANDLER.add(JoinEvent, message=TextMessage)
 def handle_join(event):
     group_id = event.source.group_id
     loggin.info(f'Obtained Group ID: {group_id}')
 
-def main():
+@app.route("/callback", methods=['POST'])
+def callback():
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
     logging.info(f'Request body: {body}')
@@ -20,4 +24,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    port = int(environ.get('PORT', 5000))
+    APP.run(host='0.0.0.0', port=port)

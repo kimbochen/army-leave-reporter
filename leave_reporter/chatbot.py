453510157d63604c63af97de786b46628ac91749
kimbochen <github@thc.anonaddy.com>
2022-01-15 13:36:12 +0800

    Altered handler signature.

--- leave_reporter/chatbot.py
+++ leave_reporter/chatbot.py
@@ -1,5 +1,4 @@
 import os
-import logging
 
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
@@ -10,16 +9,16 @@ LINE_BOT_API = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 HANDLER = WebhookHandler(os.environ['CHANNEL_SECRET'])
 
 
-@HANDLER.add(JoinEvent, message=TextMessage)
+@HANDLER.add(JoinEvent)
 def handle_join(event):
     group_id = event.source.group_id
-    loggin.info(f'Obtained Group ID: {group_id}')
+    APP.logger.info(f'Obtained Group ID: {group_id}')
 
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
-    logging.info(f'Request body: {body}')
+    APP.logger.info(f'Request body: {body}')
     HANDLER.handle(body, signature)
 
 

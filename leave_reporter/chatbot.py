5f93efb34beba0db0afea3e1b12b70ca87a703de
kimbochen <github@thc.anonaddy.com>
2022-01-15 13:59:36 +0800

    Added test message event.

--- leave_reporter/chatbot.py
+++ leave_reporter/chatbot.py
@@ -16,6 +16,11 @@ def handle_join(event):
     group_id = event.source.group_id
     app.logger.info(f'Obtained Group ID: {group_id}')
 
+@HANDLER.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    reply = event.message.text
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply))
+
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']

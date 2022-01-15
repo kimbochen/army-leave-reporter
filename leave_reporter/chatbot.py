34c306a1cab9d5ca9f3f47ba78731b72fc3712bb
kimbochen <github@thc.anonaddy.com>
2022-01-15 14:50:34 +0800

    Test logging.

--- leave_reporter/chatbot.py
+++ leave_reporter/chatbot.py
@@ -9,21 +9,20 @@ from linebot.models import JoinEvent, MessageEvent, TextMessage, TextSendMessage
 
 APP = Flask(__name__)
 LINE_BOT_API = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
+HANDLER = WebhookHandler(os.environ['CHANNEL_SECRET'])
 GROUP_ID = None
 
+
+@HANDLER.add(JoinEvent)
+def handle_join(event):
+    GROUP_ID = event.source.group_id
+    logging.info(f'Obtained Group ID: {GROUP_ID}')
 
 @APP.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
-
-    handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
-    @handler.add(JoinEvent)
-    def handle_join(event):
-        GROUP_ID = event.source.group_id
-    handler.handle(body, signature)
-    print(f'Obtained Group ID: {GROUP_ID}')
-
+    HANDLER.handle(body, signature)
     return 'OK'
 
 

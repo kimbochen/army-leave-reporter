39a6b23fe98c5edda51be9a9a73dafcd39b6ca0c
kimbochen <github@thc.anonaddy.com>
2022-01-15 13:43:27 +0800

    Added LINE API import.

--- leave_reporter/chatbot.py
+++ leave_reporter/chatbot.py
@@ -2,9 +2,10 @@ import os
 
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
+from linebot.models import JoinEvent, MessageEvent, TextMessage, TextSendMessage
 
 
-APP = Flask(__name__)
+app = Flask(__name__)
 LINE_BOT_API = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 HANDLER = WebhookHandler(os.environ['CHANNEL_SECRET'])
 
@@ -12,16 +13,16 @@ HANDLER = WebhookHandler(os.environ['CHANNEL_SECRET'])
 @HANDLER.add(JoinEvent)
 def handle_join(event):
     group_id = event.source.group_id
-    APP.logger.info(f'Obtained Group ID: {group_id}')
+    app.logger.info(f'Obtained Group ID: {group_id}')
 
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
-    APP.logger.info(f'Request body: {body}')
+    app.logger.info(f'Request body: {body}')
     HANDLER.handle(body, signature)
 
 
 if __name__ == '__main__':
     port = int(environ.get('PORT', 5000))
-    APP.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)

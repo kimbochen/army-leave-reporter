6071217cfea9bcfe3a41b5ddcc648c7cc79b564c
kimbochen <github@thc.anonaddy.com>
2022-01-15 19:46:10 +0800

    Reverted back to command-based reply.

--- leave_reporter/chatbot.py
+++ leave_reporter/chatbot.py
@@ -1,11 +1,9 @@
 import os
-import time
-from multiprocessing import Process, Array
 
 import schedule
 from flask import Flask, request
 from linebot import LineBotApi, WebhookHandler
-from linebot.models import JoinEvent, TextSendMessage
+from linebot.models import JoinEvent, MessageEvent, TextMessage, TextSendMessage
 
 from leave_reporter.server import create_report
 
@@ -13,19 +11,25 @@ from leave_reporter.server import create_report
 APP = Flask(__name__)
 LINE_BOT_API = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 HANDLER = WebhookHandler(os.environ['CHANNEL_SECRET'])
-
-INIT_ID = b'z' * 33
-GROUP_ID = Array('c', INIT_ID)
+GROUP_ID = None
 
 
 @HANDLER.add(JoinEvent)
 def handle_join(event):
-    GROUP_ID.value = event.source.group_id.encode()
+    GROUP_ID = event.source.group_id
     print(f'Obtained Group ID')
 
-@HANDLER.default()
-def default(event):
-    print(event)
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    msg = event.message.text
+    if msg == '開始回報':
+        reply = 'https://forms.gle/foKhtWvw6SC7TFV69'
+    elif msg == '彙整':
+        reply = create_report()
+    else:
+        return
+    LINE_BOT_API.reply_message(event.reply_token, TextSendMessage(text=reply))
+
 
 @APP.route('/callback', methods=['POST'])
 def callback():
@@ -35,33 +39,6 @@ def callback():
     return 'OK'
 
 
-def send_form(group_id):
-    LINE_BOT_API.push_message(group_id, TextSendMessage(text='https://forms.gle/foKhtWvw6SC7TFV69'))
-
-def send_report(group_id):
-    report = create_report()
-    LINE_BOT_API.push_message(group_id, TextSendMessage(text=report))
-
-def reminder():
-    print(f'Started reminder.')
-    group_id = GROUP_ID.value.decode()
-
-    schedule.every().day.at('18:40').do(send_form, group_id=group_id)  # Taipei Timezone!
-    schedule.every().day.at('19:40').do(send_report, group_id=group_id)  # Taipei Timezone!
-
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
-
-
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    flask_p = Process(target=APP.run, args=('0.0.0.0', port))
-    flask_p.start()
-
-    while GROUP_ID.value == INIT_ID:
-        time.sleep(1)
-    else:
-        flask_p.terminate()
-        flask_p.join()
-        reminder()
+    APP.run(host='0.0.0.0', port=port)

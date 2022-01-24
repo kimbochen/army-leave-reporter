d0a0f44458d5d4ffd49d83c3f1817ea0bb8f8a6a
kimbochen <github@thc.anonaddy.com>
2022-01-24 10:18:19 +0800

    Altered the message to make it less cheeky.

--- chatbot.py
+++ chatbot.py
@@ -13,7 +13,7 @@ REPORT_MSG = f'''回報連結：
 {os.environ['FORM_LINK']}
 確認回報情形：
 https://docs.google.com/spreadsheets/d/{os.environ["SPREADSHEET_ID"]}
-這是我的最後一次了各位'''
+最後一次回報了各位'''
 
 CADENCE_CALLS = [
     '威武', '嚴肅', '剛直', '安靜', '堅強',
